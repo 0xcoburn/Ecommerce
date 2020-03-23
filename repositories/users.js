3de45158e7ae2af931fb939639ecdb7680b8b239
Matt Coburn <mattcoburn24@gmail.com>
2020-03-23 01:34:43 -0700

    password security
    hashed passwords with random salt for a safer users.json database

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
 	constructor(filename) {
@@ -26,10 +29,32 @@ class UsersRepository {
 	// add new user to users.json
 	async create(attributes) {
 		attributes.id = this.randomId();
+
+		//get salt to go with the password
+		const salt = crypto.randomBytes(8).toString('hex');
+		//hash the password and salt together
+		const buf = await scrypt(attributes.password, salt, 64);
+		//get records
 		const records = await this.getAll();
-		records.push(attributes);
+		//add hashed password and salt to user attributes
+		const record = {
+			...attributes,
+			password : `${buf.toString('hex')}.${salt}`
+		};
+		//push hashed password and salt to users.json
+		records.push(record);
+
 		await this.writeAll(records);
-		return attributes;
+
+		return record;
+	}
+
+	async comparePasswords(saved, supplied) {
+		//destructure hashed and salt
+		const [ hashed, salt ] = saved.split('.');
+		const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+		return hashed === hashedSuppliedBuf.toString('hex');
 	}
 
 	// write all records to users.json
