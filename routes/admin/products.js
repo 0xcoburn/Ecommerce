c56ddde0d18dda5f0992c293a61ae6a568c67663
Matt Coburn <mattcoburn24@gmail.com>
2020-03-24 21:29:23 -0700

    editing and deleting a product
    handleErrors middleware now has an optional argument specifically for the productEditTemplate. Added some style for admin/products/edit

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -50,8 +50,31 @@ router.post(
 	requireAuth,
 	upload.single('image'),
 	[ requireTitle, requirePrice ],
-	handleErrors(productsEditTemplate),
-	async (req, res) => {}
+	handleErrors(productsEditTemplate, async (req) => {
+		const product = await productsRepo.getOne(req.params.id);
+		return { product };
+	}),
+	async (req, res) => {
+		const changes = req.body;
+
+		if (req.file) {
+			changes.image = req.file.buffer.toString('base64');
+		}
+
+		try {
+			await productsRepo.update(req.params.id, changes);
+		} catch (err) {
+			return res.send('Could not find item');
+		}
+
+		res.redirect('/admin/products');
+	}
 );
+
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+	await productsRepo.delete(req.params.id);
+
+	res.redirect('/admin/products');
+});
 
 module.exports = router;
